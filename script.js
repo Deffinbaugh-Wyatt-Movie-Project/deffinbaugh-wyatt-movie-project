18cbd569a16a03709d7e12aab814c26fae19d21a
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-08 14:35:54 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//call the api and get stuffs back
+            //-make pretty html
+//edit and delete button
+
+
+//form for new movie
+//movie title, rating
+//post request
+//(prevent default)
+
+
+//edit existing movie
+
+
+//delete movies
