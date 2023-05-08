08a9c5bf3a6c6a22ecdbcbd201dcef71c13b1ffa
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-08 15:12:00 -0600

    displayed in tables

--- script.js
+++ script.js
@@ -3,7 +3,33 @@
 //edit and delete button
 
 // HOW TO GRAB STUFF
-fetch("https://checker-debonair-trigonometry.glitch.me/movies").then(res => res.json()).then(data => console.log(data));
+fetch("https://checker-debonair-trigonometry.glitch.me/movies")
+    .then(res => res.json())
+    .then(data => {
+    console.log(data);
+    displayMovies(data);
+});
+
+function displayMovies(data) {
+    data.forEach(movie => {
+        display += `<tr>
+        <td>${movie.title}</td>
+        <td>${movie.genre}</td>
+        <td>${movie.director}</td>
+        <td>${movie.rating}</td>
+    </tr>`
+    })
+    document.querySelector('#display-movies').innerHTML = display;
+}
+
+let display = `<tr>
+        <th>Movie Title</th>
+        <th>Genre</th>
+        <th>Director</th>
+        <th>Rating</th>
+    </tr>`
+
+
 
 
 //form for new movie
