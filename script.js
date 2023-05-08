8d5a01b023510469e5e2729390dcf90b1678daae
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-08 14:46:11 -0600

    added ability to pull info

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
             //-make pretty html
 //edit and delete button
 
+// HOW TO GRAB STUFF
+fetch("https://checker-debonair-trigonometry.glitch.me/movies").then(res => res.json()).then(data => console.log(data));
+
 
 //form for new movie
 //movie title, rating
