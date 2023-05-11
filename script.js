1dfe048ede2ff8eb386d62884ff0c5edff045759
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-05-10 18:02:19 -0600

    fixed modals not working, cleaned up code a lil

--- script.js
+++ script.js
@@ -1,38 +1,86 @@
 //modal selectors
-const modal = document.querySelector('.modal');
 const modalOne = document.querySelector('.modal-one');
 const modalTwo = document.querySelector('.modal-two');
 const overlay = document.querySelector('.overlay');
-const closeButton = document.querySelector('.close-modal');
+const closeButtons = document.querySelectorAll('.close-modal');
 //var to hold the current button pressed
 let currentButton;
-
-
+//array to hold ids
 let idsArr = [];
-
+//array to hold movie objects for sorting
 let movies = [];
 
+//initial call to the database
 getData();
 
+//click events
+$(document).ready(function () {
+    //adding a movie
+    $("#addMovie").click(function () {
+        var title = $('#addTitle').val();
+        var genre = $('#addGenre').val();
+        var director = $('#addDirector').val();
+        var rating = $('#addRating').val();
+        addMovie(director, genre, rating, title);
+    });
 
-// HOW TO GRAB STUFF
+    //save movie
+    $('#saveMovie').click(function () {
+        var title = $('#editTitle').val();
+        var genre = $('#editGenre').val();
+        var director = $('#editDirector').val();
+        var rating = $('#editRating').val();
+        editMovie(currentButton, director, genre, rating, title);
+    });
+
+    //sort movies
+    document.querySelector("#sort").addEventListener('change', function () {
+        sort(this.value);
+    });
+
+    //bring up add movie modal
+    $('#addAMovie').click(function () {
+        showModalTwo();
+    });
+
+    //enable search button
+    $(`#search_button`).click(function () {
+        var searchInput = $('#search_bar').val();
+        let filteredMovies = movies.filter(movie => filterConditions(movie, searchInput));
+        displayMovies(filteredMovies);
+        idsArr = [];
+        filteredMovies.forEach(item => idsArr.push(item.id));
+        addEvents(idsArr);
+    });
+
+    //MODAL STUFF
+    closeButtons[0].addEventListener('click', closeModal);
+    closeButtons[1].addEventListener('click', closeModal);
+    document.addEventListener('keydown', e => {
+        if (e.key === "Escape") closeModal()
+    });
+});
+
+
+//function that fetches the data and then displays it accordingly
 function getData() {
+    //disable buttons while fetching
     setDisable();
     fetch("https://checker-debonair-trigonometry.glitch.me/movies")
         .then(res => res.json())
         .then(data => {
+            //remove loading screen and display movies
             $('#loading').addClass('hidden');
             $('#display-movies').removeClass('hidden');
             $('#addMovieSection').removeClass('hidden');
             movies = [];
             movies = data;
-            console.log(data);
             displayMovies(movies);
             idsArr = [];
             data.forEach(item => idsArr.push(item.id));
             addEvents(idsArr);
-            console.log("movies:")
-            console.log(movies);
+            clearInputs();
+            //re-enable the buttons
             setEnable()
         });
 }
@@ -40,15 +88,29 @@ function getData() {
 
 //adds event listeners to all edit and delete buttons
 function addEvents(num) {
-    //TODO change id iteration to match id's from the data instead of incrementing
     idsArr.forEach(num => {
+        //edit buttons
         $(`#edit-${num}`).click(function () {
             showModalOne();
+            //grabbing the movie id from the button
             currentButton = (this.id).slice(5);
+            //disable buttons while fetching
+            setDisable();
+            //fetching the current movie information to pre-filled in
+            fetch(`https://checker-debonair-trigonometry.glitch.me/movies/${currentButton}`)
+                .then(res => res.json())
+                .then(data => {
+                    $('#editTitle').val(`${data.title}`);
+                    $('#editGenre').val(`${data.genre}`);
+                    $('#editDirector').val(`${data.director}`);
+                    $('#editRating').val(`${data.rating}`);
+                });
+            //re-enabling buttons
+            setEnable();
         })
+        //delete buttons
         $(`#delete-${num}`).click(function () {
             currentButton = (this.id).slice(7);
-            console.log(currentButton)
             deleteMovie(currentButton);
         })
     })
@@ -60,67 +122,52 @@ function displayMovies(data) {
     // html starter template for movie display
     let display = "";
 
-    // data.forEach(movie => {
-    //     display += `<tr>
-    //     <td>${movie.title}</td>
-    //     <td>${movie.genre}</td>
-    //     <td>${movie.director}</td>
-    //     <td>${movie.rating}</td>
-    //     <td><button id="edit-${movie.id}">Edit</button></td>
-    //     <td><button id="delete-${movie.id}">Delete</button></td>
-    // </tr>`
-    // });
-
     data.forEach(movie => {
+        //creating star string for ratings
         let stars = "";
-
-        for(let i = 0; i < movie.rating; i++) {
+        for (let i = 0; i < movie.rating; i++) {
             stars += "&#9733;";
         }
 
+        //movie card
         display += `<div style="margin: 1em;">
-                <div class="card" style="width: 16rem; min-height: 100%;">
-                <div id="image-holder-${movie.id}">
-                </div>
-                <div class="card-body d-flex flex-column justify-content-between">
-                <div>
-                <h3>${movie.title}</h3>
-                <h5>${movie.director}</h5>
-                <p>${movie.genre}</p>
-                </div>
-                <div class="d-flex justify-content-between">
-                <h6>${stars}</h6>
-                <div>
-                <button id="edit-${movie.id}">Edit</button>
-                <button id="delete-${movie.id}">Delete</button>
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>`
+                        <div class="card" style="width: 16rem; min-height: 100%;">
+                            <div id="image-holder-${movie.id}"></div>
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h3>${movie.title}</h3>
+                                        <h5>${movie.director}</h5>
+                                        <p>${movie.genre}</p>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <h6>${stars}</h6>
+                                            <div>
+                                                <button class="noBorders" id="edit-${movie.id}"><i class="fa-solid fa-pen"></i></button>
+                                                <button class="noBorders" id="delete-${movie.id}"><i class="fa-solid fa-trash-can"></i></button>
+                                            </div>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>`
 
+        //getting the movie poster and appending it to the movie card
         getPoster(movie.title, `#image-holder-${movie.id}`);
     });
 
-
-
-
+    //adding the movie cards to the html
     document.querySelector('#movie-container').innerHTML = display;
 }
 
 
-
 //function to add movie
 function addMovie(director, genre, rating, title) {
-    setDisable()
+    setDisable();
     let reviewObj = {
         director,
         genre,
-        // id: startingID,
         rating,
         title
     };
-
 
     let url = 'https://checker-debonair-trigonometry.glitch.me/movies';
     const options = {
@@ -131,12 +178,19 @@ function addMovie(director, genre, rating, title) {
         body: JSON.stringify(reviewObj)
     };
     fetch(url, options)
-        .then(response => console.log(response)).then(getData) /* review was created successfully */
-        .catch(error => console.error(error)); /* handle errors */
-
-    // startingID++;
-
+        .then(getData)
+        .catch(error => console.error(error));
 }
+
+//used to reset the input fields after a movie is added
+function clearInputs() {
+    $('#addTitle').val('');
+    $('#addGenre').val('');
+    $('#addDirector').val('');
+    $('#addRating').val('');
+    closeModal();
+}
+
 
 //function to delete movie
 function deleteMovie(id) {
@@ -149,9 +203,10 @@ function deleteMovie(id) {
         }
     };
     fetch(url, options)
-        .then(response => console.log(response)).then(getData) /* review was created successfully */
-        .catch(error => console.error(error)); /* handle errors */
+        .then(getData)
+        .catch(error => console.error(error));
 }
+
 
 //function to edit movie
 function editMovie(id, director, genre, rating, title) {
@@ -172,51 +227,43 @@ function editMovie(id, director, genre, rating, title) {
         body: JSON.stringify(editObj)
     };
     fetch(url, options)
-        .then(response => console.log(response)).then(getData) /* review was created successfully */
-        .catch(error => console.error(error)); /* handle errors */
+        .then(getData)
+        .catch(error => console.error(error));
 
 }
 
-//adding a movie
-$(document).ready(function () {
-    $("#addMovie").click(function () {
-        var title = $('#addTitle').val();
-        var genre = $('#addGenre').val();
-        var director = $('#addDirector').val();
-        var rating = $('#addRating').val();
-        addMovie(director, genre, rating, title);
-    });
-});
-
-
-//save movie
-$('#saveMovie').click(function () {
-    save(currentButton);
-});
-
-function save(id) {
-    var title = $('#editTitle').val();
-    var genre = $('#editGenre').val();
-    var director = $('#editDirector').val();
-    var rating = $('#editRating').val();
-    editMovie(id, director, genre, rating, title);
-}
-
-
-document.querySelector("#sort").addEventListener('change', function () {
-    sort(this.value);
-})
 
 function sort(selected) {
+    //sort by rating
     if (selected === "rating") {
         movies.sort((a, b) => {
-            return a.rating - b.rating;
+            return b.rating - a.rating;
         });
-    } else if (selected === "title") {
+    //sort by anything else
+    } else {
         movies.sort((a, b) => {
-            let fa = a.title.toLowerCase(),
-                fb = b.title.toLowerCase();
+            let fa,fb;
+            //determine sort parameter
+            switch(selected) {
+                case "title" :
+                    fa = a.title.toLowerCase();
+                    fb = b.title.toLowerCase();
+                    break;
+                case "genre":
+                    fa = a.genre.toLowerCase();
+                    fb = b.genre.toLowerCase();
+                    break;
+                case "director":
+                    fa = a.director.toLowerCase();
+                    fb = b.director.toLowerCase();
+                    break;
+                default:
+                    fa = a.title.toLowerCase();
+                    fb = b.title.toLowerCase();
+                    break;
+            }
 
+            //perform sort
             if (fa < fb) {
                 return -1;
             }
@@ -224,34 +271,10 @@ function sort(selected) {
                 return 1;
             }
             return 0;
-        })
-    } else if (selected === "genre") {
-        movies.sort((a, b) => {
-            let fa = a.genre.toLowerCase(),
-                fb = b.genre.toLowerCase();
-
-            if (fa < fb) {
-                return -1;
-            }
-            if (fa > fb) {
-                return 1;
-            }
-            return 0;
-        })
-    } else if (selected === "director") {
-        movies.sort((a, b) => {
-            let fa = a.director.toLowerCase(),
-                fb = b.director.toLowerCase();
-
-            if (fa < fb) {
-                return -1;
-            }
-            if (fa > fb) {
-                return 1;
-            }
-            return 0;
-        })
+        });
     }
+
+    //re-display movies and reset id array so buttons still work
     displayMovies(movies);
     idsArr = [];
     movies.forEach(item => idsArr.push(item.id));
@@ -259,8 +282,7 @@ function sort(selected) {
 }
 
 
-//MODAL STUFF
-closeButton.addEventListener('click', closeModal);
+
 
 function closeModal() {
     modalOne.classList.add('hidden');
@@ -279,24 +301,6 @@ function showModalTwo() {
     overlay.classList.remove('hidden');
     overlay.addEventListener('click', closeModal);
 }
-
-document.addEventListener('keydown', e => {
-    if (e.key === "Escape") closeModal()
-});
-
-$('#addAMovie').click(function () {
-    showModalTwo();
-})
-
-$(`#search_button`).click(function () {
-    var searchInput = $('#search_bar').val();
-    let filteredMovies = movies.filter(movie => filterConditions(movie, searchInput));
-    console.log(filteredMovies)
-    displayMovies(filteredMovies);
-    idsArr = [];
-    filteredMovies.forEach(item => idsArr.push(item.id));
-    addEvents(idsArr);
-})
 
 function filterConditions(movie, searchInput) {
     let byTitle = movie.title.toLowerCase().includes(searchInput.toLowerCase());
@@ -321,8 +325,7 @@ async function getPoster(searchTerm, location) {
     const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=6c2988bd`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
-    console.log(data.Search);
-    if (data.Search != undefined) {
+    if (data.Search != undefined && data.Search[0].Poster != "N/A") {
         $(location).append(`<img src="${data.Search[0].Poster}" style="height:380px; width:255px;" class="card-img-top" alt="movie poster">`);
     } else {
         $(location).append(`<img src="img/film-projector.jpeg" class="card-img-top" alt="movie poster">`);
@@ -330,7 +333,7 @@ async function getPoster(searchTerm, location) {
 
 }
 
-// getPoster("star warhjkhjvkhjvkhvhgs:episode I", '#poster-testing');
+
 
 
 
